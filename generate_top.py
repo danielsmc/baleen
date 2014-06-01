5ed058d4e4bbbce8fe1249620b02090c7e82bf9f
Daniel McLaughlin <daniel@danielmclaughl.in>
2014-06-01 00:33:14 -0400

    return expanded_text in top json

--- generate_top.py
+++ generate_top.py
@@ -28,7 +28,7 @@ cur.execute("""select url, url_hash, sum(impact) as total_impact from
 
 out = []
 for url_row in cur.fetchall():
-    cur.execute("""select tweet_id, text, screen_name, profile_image_url from
+    cur.execute("""select tweet_id, expanded_text, screen_name, profile_image_url from
         (SELECT tweet_id, sum(impact) as total_impact FROM tweet_urls JOIN retweets USING(tweet_id) JOIN users USING(user_id)
             WHERE created_at > DATE_SUB(NOW(),INTERVAL %s MINUTE) and url_hash = %s
             group by tweet_id order by total_impact desc) as a

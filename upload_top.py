0238dd91e49bc113d1f7bc7b1fbcc71ca74f5f03
Daniel McLaughlin <daniel@danielmclaughl.in>
2014-06-01 18:04:39 -0400

    Make the html file replacement atomic

--- upload_top.py
+++ upload_top.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 
 from generate_top import generate_top
+import os
 import pystache
 import sys
+import tempfile
 
 if len(sys.argv) < 2:
 	print "pass an output filepath"
@@ -11,5 +13,8 @@ else:
 
 	data = generate_top()
 
-	with open(sys.argv[1],"w") as fh:
+	temp_fd,temp_path = tempfile.mkstemp()
+	with os.fdopen(temp_fd,'w') as fh:
 		fh.write(renderer.render_path("template.mustache",data).encode('utf8'))
+
+	os.rename(temp_path,sys.argv[1])

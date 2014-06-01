d4a96bc1feedb2f02b473512a1abf03b33da4cd2
Daniel McLaughlin <daniel@danielmclaughl.in>
2014-06-01 18:12:22 -0400

    Make the html file world readable so the web server can get at it

--- upload_top.py
+++ upload_top.py
@@ -3,6 +3,7 @@
 from generate_top import generate_top
 import os
 import pystache
+import stat
 import sys
 import tempfile
 
@@ -17,4 +18,5 @@ else:
 	with os.fdopen(temp_fd,'w') as fh:
 		fh.write(renderer.render_path("template.mustache",data).encode('utf8'))
 
+	os.chmod(temp_path,stat.S_IWUSR | stat.S_IRUSR | stat.S_IRGRP | stat.S_IROTH)
 	os.rename(temp_path,sys.argv[1])

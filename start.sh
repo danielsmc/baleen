9905606f8cfff53682d25a83d193314d22e2d4fa
Daniel McLaughlin <daniel@danielmclaughl.in>
2016-10-12 15:00:23 -0400

    more expand jobs

--- start.sh
+++ start.sh
@@ -1,5 +1,9 @@
 ./stop.sh
 
+. ve/bin/activate
+
 nohup python -u bin/stream.py &
+nohup python -u bin/expand_urls.py &
+nohup python -u bin/expand_urls.py &
 nohup python -u bin/expand_urls.py &
 nohup python -u bin/expand_urls.py &
